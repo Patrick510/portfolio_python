f2b4272acf01da8c8a98d2c409a8b82a648a202b
Patrick510 <patrickdedinho321xz@gmail.com>
2025-01-02 15:07:28 -0400

    add login functionality with authentication routes and create login template

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, jsonify
 
 nome = 'Patrick Dias'
+attempts = {}
+data = {
+    "name": "admin",
+    "password": "admin"
+}
 
 @app.route('/')
 @app.route('/index')
@@ -18,3 +23,26 @@ def contato():
 def class_page():
     title = "Classes"
     return render_template('class.html', title=title, nome=nome)
+
+@app.route("/login")
+def login():
+    title = "Login"
+    return render_template("login.html", title=title)
+
+@app.route("/auth", methods=["POST"])
+def auth():
+    user = request.form.get('user')
+    password = request.form.get('pass')
+
+    if user not in attempts:
+        attempts[user] = 0
+
+    if attempts[user] >= 2:
+        return jsonify({"message": "Too many failed attempts. Try again later."}), 403
+
+    if user == data['name'] and password == data["password"]:
+        attempts[user] = 0  # Reset attempts on success
+        return jsonify({"message": "Authenticated!", "user": user}), 200
+    else:
+        attempts[user] += 1
+        return jsonify({"message": "Invalid user or password"}), 401
